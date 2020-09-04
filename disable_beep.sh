3ce9be1e9875f45fe3f59b680bb82a730d423bb0
Rhys Williams <me@rhyswilliams.co.za>
2020-09-04 19:47:26 +0200

    Update disable_beep.sh

--- disable_beep.sh
+++ disable_beep.sh
@@ -1,3 +1,4 @@
+# * * * * * /bin/sh /opt/scripts/disablebeeperups.sh >> /opt/scripts/disablebeeperups.log
 if upsc ups ups.beeper.status | grep -q "enabled";
 then
     echo $(date) "Beeper Enabled. Disabling."
